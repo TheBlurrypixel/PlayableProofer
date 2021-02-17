15c873df8717b0f99905ad9aa065fde2e222c367
Michael Gochoco <mgochoco@yahoo.com>
2021-02-16 22:36:40 -0800

    Moved to build folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var browserSync = require('browser-sync').create();
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "."
+            baseDir: "./build"
         }
     });
 
-    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("./build/*.html").on('change', browserSync.reload);
 });
 gulp.task('default', gulp.series(['browser-sync']));
